79cade640c98f8064f4e9727170ec58c47894814
freshbreeze <hodu@zhaw.ch>
2013-06-19 13:43:55 +0200

    stable version 1.5

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2013051700;
+$plugin->version   = 2013061900;
 $plugin->requires  = 2012120300;
 $plugin->component = 'theme_simple';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.4';
+$plugin->release   = '1.5';
